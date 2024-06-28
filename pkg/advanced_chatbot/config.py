f19603a47d8794f80b524953aac3595c510258d0
guy <guy-stephane.waffo@mazars.fr>
2024-06-28 13:51:13 +0200

    Adding Llama 8b 4bit and bge large support

--- pkg/advanced_chatbot/config.py
+++ pkg/advanced_chatbot/config.py
@@ -14,6 +14,6 @@ DEFAULT_RAG_TOKEN_LIMIT = 2000
 
 
 #USE FAKE LLMS
-USE_MOCK_MODELS = False
-USE_LOCAL_MODELS = False # If true, use bge large for embedding, llama3_8b 4bit for generation
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+USE_MOCK_MODELS = True 
+USE_LOCAL_MODELS = False # If true (and mock model_disabled), llm=llama7b, embedding=bge_large.
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY") 
